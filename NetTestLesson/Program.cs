64226570c55fed9e51f561680a3f5745e39261ff
AlexanderLukashuk <upiter465@gmail.com>
2020-11-11 21:08:12 +0600

    Работа с webClient. Введение в сети

---
+++ NetTestLesson/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace NetTestLesson
+{
+    class Program
+    {
+        private static string data = string.Empty;
+        static async Task Main(string[] args)
+        {
+            using (var webClient = new WebClient())
+            {
+                //webClient.DownloadStringCompleted += StringDowload;
+                //webClient.DownloadStringAsync(new Uri("https://mail.ru"));
+
+                //webClient.DownloadStringTaskAsync("https://mail.ru");
+                
+                Console.WriteLine(await webClient.DownloadStringTaskAsync("https://mail.ru"));
+                Console.ReadLine();
+            }
+
+            var webRequest = WebRequest.Create(("https://mail.ru"));
+            webRequest.Method = "GET";
+            var webResponse = webRequest.GetResponse();
+            webResponse.Close();
+
+            //var httpRecuest = HttpWebRequest.Create("https://mail.ru");
+        }
+
+        private static void StringDowload(object sender, DownloadStringCompletedEventArgs e)
+        {
+            data = e.Result;
+        }
+    }
+}
